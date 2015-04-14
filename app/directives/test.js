8c58f5d8557f6b2b8611a8d580f2092d8545b127
otelnov <otelnov@gmail.com>
2015-04-14 12:10:25 +0300

    html and css loaders

--- app/directives/test.js
+++ app/directives/test.js
@@ -1,1 +1,16 @@
-console.log(123);
+export default ngModule => {
+  ngModule.directive('test', () => {
+    require('./test.css');
+    return {
+      restrict: 'E',
+      scope: {},
+      template: require('./test.html'),
+      controllerAs: 'vm',
+      controller: function () {
+        const vm = this;
+
+        vm.test = 'test';
+      }
+    }
+  });
+};
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel',
         exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+        exclude: /node_modules/
       }
     ]
   }
